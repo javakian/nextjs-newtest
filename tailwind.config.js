9795753ecdfeef6bc10c650f694edbac11691299
James L. Avakian, Software Developer <javakian@users.noreply.github.com>
2021-06-18 19:31:56 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-    mode: 'jit',
-   purge: [],
-   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
